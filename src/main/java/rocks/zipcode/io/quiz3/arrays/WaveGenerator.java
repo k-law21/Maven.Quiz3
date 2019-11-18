1835c237f331b695910eb6dcfe52106a08e95ffd
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-18 16:04:01 -0500

    Wave Generator done

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,26 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        str = str.toLowerCase();
+        List<String> strList = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 65 && c<= 90 || c >= 97 && c <= 122) {
+                StringUtils stringUtils = new StringUtils();
+                strList.add(stringUtils.capitalizeNthCharacter(str, i));
+            }
+        }
+        String[] strArr = new String[strList.size()];
+        return strList.toArray(strArr);
     }
 }
+
